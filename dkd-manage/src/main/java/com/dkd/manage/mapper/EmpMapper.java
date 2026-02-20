787674b83dbab4335102ad4b4e3d61dbafa62cce
zhouming <minerchow@126.com>
2026-02-20 13:17:22 +0800

    update

---
+++ dkd-manage/src/main/java/com/dkd/manage/mapper/EmpMapper.java
@@ -0,0 +1,72 @@
+package com.dkd.manage.mapper;
+
+import java.util.List;
+import com.dkd.manage.domain.Emp;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
+
+/**
+ * 人员列表Mapper接口
+ * 
+ * @author ruoyi
+ * @date 2026-02-19
+ */
+public interface EmpMapper 
+{
+    /**
+     * 查询人员列表
+     * 
+     * @param id 人员列表主键
+     * @return 人员列表
+     */
+    public Emp selectEmpById(Long id);
+
+    /**
+     * 查询人员列表列表
+     * 
+     * @param emp 人员列表
+     * @return 人员列表集合
+     */
+    public List<Emp> selectEmpList(Emp emp);
+
+    /**
+     * 新增人员列表
+     * 
+     * @param emp 人员列表
+     * @return 结果
+     */
+    public int insertEmp(Emp emp);
+
+    /**
+     * 修改人员列表
+     * 
+     * @param emp 人员列表
+     * @return 结果
+     */
+    public int updateEmp(Emp emp);
+
+    /**
+     * 删除人员列表
+     * 
+     * @param id 人员列表主键
+     * @return 结果
+     */
+    public int deleteEmpById(Long id);
+
+    /**
+     * 批量删除人员列表
+     * 
+     * @param ids 需要删除的数据主键集合
+     * @return 结果
+     */
+    public int deleteEmpByIds(Long[] ids);
+
+    /**
+     * 更新人员列表区域信息
+     * 
+     * @param emp 人员列表
+     * @return 结果
+     */
+    @Update("update tb_emp set region_name = #{regionName} where region_id = #{id}")
+    public int updateRegionById(@Param("regionName") String regionName, @Param("id") Long id);
+}
---
+++ dkd-manage/src/main/java/com/dkd/manage/service/impl/EmpServiceImpl.java
@@ -0,0 +1,134 @@
+package com.dkd.manage.service.impl;
+
+import java.util.List;
+import com.dkd.common.utils.DateUtils;
+import com.dkd.manage.domain.Role;
+import com.dkd.manage.domain.Region;
+import com.dkd.manage.mapper.RegionMapper;
+import com.dkd.manage.mapper.RoleMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.dkd.manage.mapper.EmpMapper;
+import com.dkd.manage.domain.Emp;
+import com.dkd.manage.service.IEmpService;
+
+/**
+ * 人员列表Service业务层处理
+ * 
+ * @author ruoyi
+ * @date 2026-02-19
+ */
+@Service
+public class EmpServiceImpl implements IEmpService 
+{
+    @Autowired
+    private EmpMapper empMapper;
+    @Autowired
+    private RegionMapper regionMapper;
+
+    @Autowired
+    private RoleMapper roleMapper;
+    /**
+     * 查询人员列表
+     * 
+     * @param id 人员列表主键
+     * @return 人员列表
+     */
+    @Override
+    public Emp selectEmpById(Long id)
+    {
+        return empMapper.selectEmpById(id);
+    }
+
+    /**
+     * 查询人员列表列表
+     * 
+     * @param emp 人员列表
+     * @return 人员列表
+     */
+    @Override
+    public List<Emp> selectEmpList(Emp emp)
+    {
+        return empMapper.selectEmpList(emp);
+    }
+
+    /**
+     * 新增人员列表
+     * 
+     * @param emp 人员列表
+     * @return 结果
+     */
+    @Override
+    public int insertEmp(Emp emp)
+    {
+        //补充区域名称
+        Long regionId = emp.getRegionId();
+        if(regionId != null) {
+            Region region = regionMapper.selectRegionById(regionId);
+            if(region != null) {
+                emp.setRegionName(region.getRegionName());
+            }
+        }
+        //补充角色信息 先拿到角色对象，再补充角色名称
+        Role role = roleMapper.selectRoleByRoleId(emp.getRoleId());
+        if(role != null){
+            emp.setRoleName(role.getRoleName());
+            emp.setRoleCode(role.getRoleCode());
+        }
+       
+
+        emp.setCreateTime(DateUtils.getNowDate());
+        return empMapper.insertEmp(emp);
+    }
+
+    /**
+     * 修改人员列表
+     * 
+     * @param emp 人员列表
+     * @return 结果
+     */
+    @Override
+    public int updateEmp(Emp emp)
+    {
+        //补充区域名称
+        Long regionId = emp.getRegionId();
+        if(regionId != null) {
+            Region region = regionMapper.selectRegionById(regionId);
+            if(region != null) {
+                emp.setRegionName(region.getRegionName());
+            }
+        }
+        //补充角色信息 先拿到角色对象，再补充角色名称
+        Role role = roleMapper.selectRoleByRoleId(emp.getRoleId());
+        if(role != null){
+            emp.setRoleName(role.getRoleName());
+            emp.setRoleCode(role.getRoleCode());
+        }
+        emp.setUpdateTime(DateUtils.getNowDate());
+        return empMapper.updateEmp(emp);
+    }
+
+    /**
+     * 批量删除人员列表
+     * 
+     * @param ids 需要删除的人员列表主键
+     * @return 结果
+     */
+    @Override
+    public int deleteEmpByIds(Long[] ids)
+    {
+        return empMapper.deleteEmpByIds(ids);
+    }
+
+    /**
+     * 删除人员列表信息
+     * 
+     * @param id 人员列表主键
+     * @return 结果
+     */
+    @Override
+    public int deleteEmpById(Long id)
+    {
+        return empMapper.deleteEmpById(id);
+    }
+}
